bd007bed12f0f0a644ea1bcbd1d3172711bb577c
Gerardo Ramos <gerardoere02@gmail.com>
2023-11-20 01:46:58 -0600

    prueba

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <div class="w-full h-screen flex flex-col justify-center items-center">
     <h1 class="text-4xl">GERARDO RAMOS</h1>
     <p class="text-2xl">Work in progress...</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt veritatis laborum sequi ipsum magnam eum? Eos aperiam ullam sunt possimus asperiores itaque, omnis quod commodi mollitia illum. Numquam, veritatis totam.</p>
 </div>
