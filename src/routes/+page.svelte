10961db840f9b2ccd9666adecec4f1ddbd6b3036
Gerardo Ramos <gerardoere02@gmail.com>
2023-11-20 00:08:27 -0600

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-screen flex flex-col justify-center items-center">
+    <h1 class="text-4xl">GERARDO RAMOS</h1>
+    <p class="text-2xl">Work in progress...</p>
+</div>
